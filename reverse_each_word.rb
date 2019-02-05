9a30daf6814135a53d92ba31e1593778638270d2
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-05 19:26:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   return sentence.split(" ")
   new_sentence = []
-  new_sentence = sentence.collect.push { |word| return word.reverse }
+  new_sentence = sentence.collect.push { |word| return word.reverse! }
   return new_sentence
 binding.pry
 end
