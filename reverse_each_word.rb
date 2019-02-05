29c9cde7ecb461634f1ec527f56b6c01b95ac264
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-05 19:25:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 require 'pry'
 def reverse_each_word(sentence)
   return sentence.split(" ")
-  new_sentence = sentence.collect { |word| return word.reverse }
+  new_sentence = []
+  new_sentence = sentence.collect.push { |word| return word.reverse }
   return new_sentence
 binding.pry
 end
